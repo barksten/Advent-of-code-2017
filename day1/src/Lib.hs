f975df031b0d0f5621fc48f11da2c79ac883487f
Anders Östlin <anders.ostlin@lm.se>
2017-12-04 13:49:23 +0100

    Kladd...

--- day1/src/Lib.hs
+++ day1/src/Lib.hs
@@ -1,6 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
     ( someFunc
+    , foo
     ) where
 
+
+import qualified Data.Text.IO as T
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = T.putStrLn "someFunc"
+
+foo :: Integer -> Integer -> Integer
+
+foo x y = x + y
+
+-- A thin monadic skin layer
+getList :: IO [Char]
+getList = fmap take5 getContents
+
+-- The actual worker
+take5 :: [Char] -> [Char]
+take5 = take 5 . filter (`elem` ['a'..'e'])
--- day1/test/Spec.hs
+++ day1/test/Spec.hs
@@ -1,2 +1,12 @@
+import Data.Char
+import Test.QuickCheck
+import Lib
+
+
 main :: IO ()
 main = putStrLn "Test suite not yet implemented"
+
+
+instance Arbitrary Char where
+    arbitrary     = choose ('\32', '\128')
+    coarbitrary c = variant (ord c `rem` 4)
