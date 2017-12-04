f7b81f6d63af8093af589d67e69edaba8cbe8e9d
barksten <anders.ostlin@gmail.com>
2017-12-04 21:41:42 +0100

    Day 1 part 2

--- Day 1.hsproj/Main.hs
+++ Day 1.hsproj/Main.hs
@@ -26,5 +26,14 @@ isNeighbors (x, y) = x == y
 day1 :: String -> Int
 day1 = sum . map fst . filter isNeighbors . pair . prepare
 
+rotate _ [] = []
+rotate n xs = zipWith const (drop n (cycle xs)) xs
 
+halfLength xs = quot (length xs) 2
+
+pair' :: [a] -> [(a, a)]
+pair' xs = zip xs (rotate (halfLength xs) xs)
+
+
+day1' = sum . map fst . filter isNeighbors . pair' . prepare
 
