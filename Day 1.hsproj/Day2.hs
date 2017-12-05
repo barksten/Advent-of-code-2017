adaee5c698ad58144a761a271372ad91abd77b4a
Anders Östlin <anders.ostlin@lm.se>
2017-12-05 12:42:02 +0100

    Fixa io day2

--- Day 1.hsproj/Day2.hs
+++ Day 1.hsproj/Day2.hs
@@ -1,7 +1,12 @@
 
+
 module Day2 where
 
+import Data.Char
 
+main = interact someFunc
+
+someFunc s = show (day2 s)
 
 prep :: String -> [[Int]]
 prep s = [[5,1,9,5],
@@ -9,10 +14,19 @@ prep s = [[5,1,9,5],
          [2,4,6,8]]
 
 diff :: (Num a, Ord a) => [a] -> a
-diff xs = (maximum xs) - (minimum xs)
+diff xs = maximum xs - minimum xs
 
 rowdiffer :: [[Int]] -> [Int]
-rowdiffer matrix = map diff matrix
+rowdiffer = map diff
 
 day2 :: String -> Int
-day2 = sum . rowdiffer . prep
+day2 = sum . rowdiffer . makeMatrix
+
+
+input = "123\t234\t345\t456\n123\t234\t345\t456\n123\t234\t345\t456\n"
+
+makeMatrix :: String -> [[Int]]
+makeMatrix =  map (map parseInt . words) . lines
+
+parseInt :: String -> Int
+parseInt s = read s :: Int
