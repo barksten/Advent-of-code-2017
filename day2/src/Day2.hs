e6f79ec5c1a5d27d475a409ded8a041734af857f
barksten <anders.ostlin@gmail.com>
2017-12-07 19:47:02 +0100

    Day2 part 2

--- day2/src/Day2.hs
+++ day2/src/Day2.hs
@@ -2,8 +2,8 @@ module Day2
     ( someFunc
     ) where
 
-import Data.List
-import Data.Maybe
+import           Data.List
+import           Data.Maybe
 
 
 type Row = [Int]
@@ -12,7 +12,7 @@ type Matrix = [Row]
 someFunc s = show (day2 s)
 
 day2 :: String -> Int
-day2 = sum . map divide . map evenly . map sortRow . makeMatrix
+day2 = sum . map ((divide . evenly) . sortRow) . makeMatrix
 
 makeMatrix :: String -> Matrix
 makeMatrix =  map (map parseInt . words) . lines
@@ -21,25 +21,22 @@ parseInt :: String -> Int
 parseInt s = read s :: Int
 
 sortRow :: Row -> Row
-sortRow = reverse . sort 
+sortRow = sortBy (flip compare)
 
 
 findEvenly :: Int -> Row -> Row
-findEvenly _ [] = [0]
-findEvenly n (x:xs) = if (rem n x) == 0 then [n, x] else findEvenly n xs
+findEvenly _ []     = [0]
+findEvenly n (x:xs) = if rem n x == 0 then [n, x] else findEvenly n xs
 
 evenly :: Row -> Row
 evenly [] = []
-evenly (x:xs) | (findEvenly x xs) == [0] = evenly xs
+evenly (x:xs) | findEvenly x xs == [0] = evenly xs
               | otherwise = findEvenly x xs
 
-
---if (findEvenly x xs) == 0 then evenly xs else findEvenly x xs 
-
 divide :: Row -> Int
-divide [] = 0
+divide []     = 0
 divide (x:xs) = if null xs then 0 else quot x (head xs)
-   
+
 
 
 rowdiffer :: Matrix -> Row
